48b4ea40ecf88ff183f61be3cb44f722263f126d
Your Name <you@example.com>
2024-04-10 10:15:23 +0300

    "Check"

--- main.py
+++ main.py
@@ -29,3 +29,58 @@
 # Vārdnīcas - https://www.w3schools.com/python/python_dictionaries.asp
 # Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
 #
+import json
+Purchases = []
+
+with open('Purchases.json', 'r') as openfile:
+    # Reading from json file
+    Purchases = json.load(openfile)
+# Ļauj lietotājam pieejamo līdzekļu jomā izvēlēties programmu.
+while True:
+    print("\nThe check:")
+    print("1. Pievienot produktu")
+    print("2. Dzēst produktu no čeka")
+    print("3. atcelt ievadu / iztukšot preču sarakstu")
+    print("4. piemērot atlaidi, ievadīt summu procentos")
+    print("5. beidz")
+    choice = input("Enter your choice: ")
+
+    Purchases_file = open('Purchases.json') # opening JSON file
+    Purchases = json.load(Purchases_file) # returns JSON object as a dictionary
+    Purchases_file.close()
+    # Ļauj lietotājam pievienot produktu un tā cenu čekam.
+    if choice == "1":
+        Purchase_title = input("Enter purchase title: ")
+        Purchase_price = int(input("Enter purchase price: "))
+        thisdict = {
+            "Purchase": Purchase_title,
+            "Purchase price": Purchase_price,
+            }
+        Purchases.append(thisdict)
+        print(Purchases)
+        with open("Purchases.json", "w") as outfile:
+            json.dump(Purchases, outfile)
+        pass
+    # Ļauj ar indeksa palīdzību izdzest produktu no čeka.
+    elif choice == "2":
+        id = int(input("Enter the index of purchase the  to remove: "))
+        id = id - 1 
+        Purchases.pop(id)
+        print(Purchases)
+        pass
+    # Ļauj notīrīt čeku.
+    elif choice == "3":
+       
+        Purchases.clear()
+        print(Purchases)
+        with open("Purchases.json", "w") as outfile:
+            json.dump(Purchases, outfile)
+        print(Purchases)
+        pass
+    # Ļauj ievadīt pircēja atlaidi.
+    elif choice == "4":
+        sale = int(input("Enter your buyer sale (%): "))
+        pass
+    # Aizver programmu.
+    elif choice == "5":
+        break
